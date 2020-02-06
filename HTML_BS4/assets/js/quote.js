21343cf24d76230c8e426057ddddb8ea79eccdf2
prisca-ctrl <57603629+prisca-ctrl@users.noreply.github.com>
2020-02-05 19:21:05 -0500

    vs

--- HTML_BS4/assets/js/quote.js
+++ HTML_BS4/assets/js/quote.js
@@ -1,32 +1,44 @@
-function showHide(params) {
-    var buildingty = document.getElementById("buildingtype");
-if(var buildingty===residential) {
+// function showHide(params) {
+//     var buildingty = document.getElementById("buildingtype");
+// if(var buildingty===residential) {
 
-}
+// }
 
+// }
 
-}
-
-function showResidential(params) {
+// function showResidential(params) {
 
     
-}
+// }
 
-function ShowForm(name) {
-    // this function hides all forms, and then shows the form we want
-    $('form > div').css('display', 'none');
-    $('form > #'+name).css('display', 'block');
-}
+// function ShowForm(name) {
+//     // this function hides all forms, and then shows the form we want
+//     $('form > div').css('display', 'none');
+//     $('form > #'+name).css('display', 'block');
+// }
 
-var defaultForm = 'residential';
-var currentForm = defaultForm;
+// var defaultForm = 'residential';
+// var currentForm = defaultForm;
 
-$(document).ready(function(e) {
+// $(document).ready(function(e) {
 
-    ShowForm('residential');
+//     ShowForm('residential');
 
-    $('select').change(function(e){
-        currentForm = e.name;
-        ShowForm(currentForm);
-    });
+//     $('select').change(function(e){
+//         currentForm = e.name;
+//         ShowForm(currentForm);
+//     });
+// });
+
+
+$(function () {
+    $("#res1").hide();
+    $("#selecBuil").change(selecType);
 });
+
+function selecType(){
+    if ($("#selecBuil").val() =="residential") {
+        $("#res1").show();
+        
+    }
+}
