b5c22f36389187f6b5629a818da014ce3a721fee
prisca-ctrl <57603629+prisca-ctrl@users.noreply.github.com>
2020-02-06 05:32:20 -0500

    FROM

--- HTML_BS4/assets/js/quote.js
+++ HTML_BS4/assets/js/quote.js
@@ -46,13 +46,48 @@ function selecType(){
 }
 
 
-function calculateLiftCages(params) {
+function calculateShaftNumber(params) {
    if( $("#selectBuiling").val() == "residential" ){
     var resApartmentNumber =  $("#resApartmentNumber").val();
     var resFloorNumber = $("#resFloorNumber").val();
     var resBasementNumber = $("#resBasementNumber").val();
-    var numberOfCages = resApartmentNumber / (resFloorNumber - resBasementNumber);
-    alert("numberOfCages = "+ numberOfCages);
+    var doorPerFloor  = resApartmentNumber / (resFloorNumber - resBasementNumber);
+    alert("calculateShaftNumber = "+ NumberOfCages);
    }
 }
+
+/*Residential
+for (doorPerFloor = 0; doorPerFloor < 6; doorPerFLoor){
+    var numbeOfShaft = 0;
+    doorPerFloor == 6 1shaft
+    var number of colum = doorPerFloor > 20  = +colum shafts*2  
+}*/
+
+/*commercial
+for (doorPerFloor = 0; estimatedShaftNumber == specified number
+}*/
+
+id= corporateHybride
+/*Corporate
+ var totalOccupantNumber = (floorOccupantNumber + basementNumber)* floorNumber
+ var floorNumber
+ var columsRequired = totalStoriesNumber (storiesNumber + basementNumber)/20
+ var elevatorShaftNumber
+ var columsNumber
+ var totalElevatorNumber = elvatorNumber (occupantNumber/1000)/ columsNumber
+}*/
+
+/*Hybride{
+ var totalOccupantNumber = (floorOccupantNumber + basementNumber)* floorNumber
+ var floorNumber
+ var columsRequired = totalStoriesNumber (storiesNumber + basementNumber)/20
+ var elevatorShaftNumber
+ var columsNumber
+ var totalElevatorNumber = elvatorNumber (occupantNumber/1000)/ columsNumber
+}
+
+Ready-only field; display ; recommended Number of elevators
+updating on change related fields
+
+estimatedBudget = unitPrice * cagesNumber
 
